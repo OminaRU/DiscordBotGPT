80529f44611f70bb0ba29924ae01d044e17e8da6
Omina Elsheikh <ominaelsheikh94@gmail.com>
2023-02-02 23:22:43 -0500

    testing out keys

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-print("sweet")
+from app.discord_bot.discordbot_api import client, discord_token
 
 
-https://discord.com/api/oauth2/authorize?client_id=1070896486268813413&permissions=8&scope=bot
+if __name__ == '__main__':
+    client.run(discord_token)
+
+
+
+##https://discord.com/api/oauth2/authorize?client_id=1070896486268813413&permissions=8&scope=bot

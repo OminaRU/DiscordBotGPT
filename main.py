9b0bbf646093b108c18e2e10fc4a5361ee83db1c
Omina Elsheikh <ominaelsheikh94@gmail.com>
2023-02-03 11:54:48 -0500

    running program

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-from app.discord_bot.discordbot_api import client, discord_token
+from app.discord_bot.discordbot_api import MyClient, discord_token
 
 
 if __name__ == '__main__':
-    client.run(discord_token)
+    MyClient.run(discord_token)
 
-
-
-##https://discord.com/api/oauth2/authorize?client_id=1070896486268813413&permissions=8&scope=bot
